7d9f16cff54d2428411edd2b948b369e2f11b13a
chuckstory <poowadon.ingeni@gmail.com>
2019-04-06 10:35:47 +0700

    Third Commit

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,16 @@
+
+
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength=4, ErrorMessage = "You must specify password between 4 and 8 charector")]
+        public string Password {get; set; }
+    }
+}
